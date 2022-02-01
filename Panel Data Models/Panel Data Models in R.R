d74e195e94dbe15fa9ced7760e556f80525773e7
Shanmukh dara <67149839+shanmukhdara@users.noreply.github.com>
2022-01-31 21:11:19 -0800

    Add files via upload

---
+++ Panel Data Models/Panel Data Models in R.R
@@ -0,0 +1,41 @@
+# install required packages
+install.packages("plm")
+#attaching them 
+library(plm)
+
+data_wage<- read.csv("panel_wage.csv")
+attach(data_wage)
+
+Y <- cbind(lwage)
+X <- cbind(exp, exp2, wks, ed)
+
+# convert data into a panel data object 
+pdata <- pdata.frame(data_wage, index=c("id","t"))
+
+# Descriptive statistics
+summary(Y)
+summary(X)
+
+# Lets start with Pooled model 
+pooling <- plm(Y ~ X, data=pdata, model= "pooling")
+summary(pooling)
+
+# Fixed effects model
+fixed <- plm(Y ~ X, data=pdata, model= "within")
+summary(fixed)
+
+# Random effects model
+random <- plm(Y ~ X, data=pdata, model= "random")
+summary(random)
+
+# LM test for random effects versus OLS
+plmtest(pooling)
+#significant effects supporting random effects
+
+# LM test for fixed effects versus OLS
+pFtest(fixed, pooling)
+#significant effects supporting fixed effects
+
+# Hausman test for fixed versus random effects model
+phtest(random, fixed)
+# One of the model is inconsistent, so go with fixed effects model
